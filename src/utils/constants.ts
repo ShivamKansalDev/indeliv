e62ea227d9958dbcb12287eb69300a0f45cb9d95
ShivamKansalDev <xeam.kulbir@gmail.com>
2024-05-28 16:14:04 +0530

    URL switched abc >> xyz

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 
 export const HOST = {
     production: window.location.host.includes('.vercel.app')?'https://abc.indelivtest.in':"https://" + window.location.host,
-    development:'https://abc.indelivtest.in',
+    development:'https://xyz.indelivtest.in',
     test:'https://abc.indelivtest.in',
 };
 export const TOKEN_STORAGE = 'token'
